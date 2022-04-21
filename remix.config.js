545a664fdc024ac8bdd0110b98b519e3c61b0062
Gabo Esquivel <contact@gaboesquivel.com>
2022-04-20 21:48:40 -0600

    fix: set build target to vercel

--- remix.config.js
+++ remix.config.js
@@ -2,9 +2,14 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  serverBuildTarget: 'vercel',
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
   ignoredRouteFiles: ['.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  // serverBuildPath: "build/index.js",
+  // serverBuildPath: "api/index.js",
   // publicPath: "/build/",
 }
