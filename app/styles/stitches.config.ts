550e2b03502998d02d7349bf8f3a6bc2bf0b6082
mledezmac <marcoledezmacordero09@gmail.com>
2022-04-24 14:01:28 -0600

    feat: stitches globalStyles and utils

--- app/styles/stitches.config.ts
+++ app/styles/stitches.config.ts
@@ -1,3 +1,4 @@
+import type * as Stitches from '@stitches/react';
 import { createStitches } from '@stitches/react'
 import lightTheme from './light-theme.json'
 import darkTheme from './dark-theme.json'
@@ -5,15 +6,17 @@ import darkTheme from './dark-theme.json'
 console.log('Light Theme for Stitches\n', lightTheme)
 console.log('Dark Theme for Stitches\n', darkTheme)
 
-const stitches = createStitches({
+export const { styled, globalCss, getCssText, config }  = createStitches({
   theme: {
     // colors,
-    colors: {
-      red: '#ff6d6d',
-      steel: '#363645b0',
-      black: '#000',
-      white: '#fff',
-      grey: '#666',
+    colors: lightTheme.colors,
+    sizes: {
+      container: '78.5rem',
+    },
+    fontWeights: {
+      body: 400,
+      heading: 700,
+      bold: 700,
     },
     space: {
       'space-1': '20px'
@@ -24,8 +27,73 @@ const stitches = createStitches({
     desktopUp: '(min-width: 1024px)',
     largeDesktopUp: '(min-width: 1680px)',
   },
+  utils: {
+    p: (value: Stitches.PropertyValue<'padding'>) => ({
+      padding: value,
+    }),
+    pt: (value: Stitches.PropertyValue<'paddingTop'>) => ({
+      paddingTop: value,
+    }),
+    pr: (value: Stitches.PropertyValue<'paddingRight'>) => ({
+      paddingRight: value,
+    }),
+    pb: (value: Stitches.PropertyValue<'paddingBottom'>) => ({
+      paddingBottom: value,
+    }),
+    pl: (value: Stitches.PropertyValue<'paddingLeft'>) => ({
+      paddingLeft: value,
+    }),
+    px: (value: Stitches.PropertyValue<'paddingLeft'>) => ({
+      paddingLeft: value,
+      paddingRight: value,
+    }),
+    py: (value: Stitches.PropertyValue<'paddingTop'>) => ({
+      paddingTop: value,
+      paddingBottom: value,
+    }),
+
+    m: (value: Stitches.PropertyValue<'margin'>) => ({
+      margin: value,
+    }),
+    mt: (value: Stitches.PropertyValue<'marginTop'>) => ({
+      marginTop: value,
+    }),
+    mr: (value: Stitches.PropertyValue<'marginRight'>) => ({
+      marginRight: value,
+    }),
+    mb: (value: Stitches.PropertyValue<'marginBottom'>) => ({
+      marginBottom: value,
+    }),
+    ml: (value: Stitches.PropertyValue<'marginLeft'>) => ({
+      marginLeft: value,
+    }),
+    mx: (value: Stitches.PropertyValue<'marginLeft'>) => ({
+      marginLeft: value,
+      marginRight: value,
+    }),
+    my: (value: Stitches.PropertyValue<'marginTop'>) => ({
+      marginTop: value,
+      marginBottom: value,
+    }),
+
+    bg: (value: Stitches.PropertyValue<'backgroundColor'>) => ({
+      backgroundColor: value,
+    }),
+
+    size: (value: Stitches.PropertyValue<'width'>) => ({
+      width: value,
+      height: value,
+    }),
+  }
 })
 
-const { styled, globalCss, getCssText } = stitches
+export type CSS = Stitches.CSS<typeof config>;
 
-export { styled, getCssText, globalCss }
+export const globalStyles = globalCss({
+  body: {
+    margin: 0,
+  },
+  '*, *::before, *::after': {
+    boxSizing: 'border-box',
+  },
+})
