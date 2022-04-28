baa2ed32f4e604d9728bcf5efb49e7c257096ad2
Gabo Esquivel <contact@gaboesquivel.com>
2022-04-28 17:20:48 -0600

    devops: explicit vercel server

--- remix.config.js
+++ remix.config.js
@@ -1,12 +1,15 @@
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
+const is_vercel = Boolean(process.env.VERCEL)
+const is_dev = Boolean(process.env.NODE_ENV === 'development')
+
 module.exports = {
-  serverBuildTarget: 'vercel',
+  serverBuildTarget: is_vercel ? 'vercel' : 'node-cjs',
   // When running locally in development mode, we use the built in remix
   // server. This does not understand the vercel lambda module format,
   // so we default back to the standard build output.
-  server: process.env.NODE_ENV === 'development' ? undefined : './server.js',
+  server: is_vercel ? './vercel-server.js' : undefined,
   ignoredRouteFiles: ['.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
