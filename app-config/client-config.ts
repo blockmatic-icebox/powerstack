25e7e23c96b46921ee2c51c98fbcbfef86f4427d
Gabo Esquivel <contact@gaboesquivel.com>
2022-08-12 09:15:09 -0600

    wip: fix client-config environment var

--- app-config/client-config.ts
+++ app-config/client-config.ts
@@ -3,16 +3,16 @@ import { AppConfig } from '../app-engine/types/app-config'
 export const client_args: AppConfig = {
   contracts: {},
   services: {
-    auth_api: process.env.AUTH_API || '',
-    graphql_api: process.env.GRAPHQL_API || '',
-    cloudinary_cloud_name: process.env.CLOUDINARY_CLOUD_NAME || '',
-    cloudinary_api_key: process.env.CLOUDINARY_API_KEY || '',
-    cloudinary_api_secret: process.env.CLOUDINARY_API_SECRET || '',
-    web3auth_client_id: process.env.WEB3AUTH_CLIENT_ID || '',
+    auth_api: process.env.NEXT_PUBLIC_AUTH_API || '',
+    graphql_api: process.env.NEXT_PUBLIC_GRAPHQL_API || '',
+    cloudinary_cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME || '',
+    cloudinary_api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY || '',
+    cloudinary_api_secret: process.env.NEXT_PUBLIC_CLOUDINARY_API_SECRET || '',
+    web3auth_client_id: process.env.NEXT_PUBLIC_WEB3AUTH_CLIENT_ID || '',
   },
   analytics: {
-    google: process.env.GOOGLE_ANALYTICS || '',
-    sentry: process.env.SENTRY_DSN || '',
+    google: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS || '',
+    sentry: process.env.NEXT_PUBLIC_SENTRY_DSN || '',
   },
   features: {},
   messages: {
