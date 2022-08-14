56851d4555c7bc5f7d64ea7c6f2c7d1e71a7fc2f
Gabo Esquivel <contact@gaboesquivel.com>
2022-08-14 11:45:04 -0600

    wip: authorized apollo client

--- pages/account.tsx
+++ pages/account.tsx
@@ -38,7 +38,7 @@ const ButtonGroup = styled('div', {
 // })
 
 const Home: NextPage = () => {
-  const { createAccount } = useAppEngine()
+  const { createUserAccount } = useAppEngine()
   const [username, setUsername] = useState('')
   return (
     <LoginBackground>
@@ -53,7 +53,7 @@ const Home: NextPage = () => {
             css={{ maxWidth: 450, width: '100%' }}
           />
           <ButtonGroup>
-            <Button onClick={() => createAccount(username)}>Create your Account</Button>
+            <Button onClick={() => createUserAccount(username)}>Create your Account</Button>
           </ButtonGroup>
         </Container>
       </MainContent>
