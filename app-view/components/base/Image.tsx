ef3f7c46c9a6968333ecf72581acea5489c1388b
mledezmac <marcoledezmacordero09@gmail.com>
2022-08-19 06:26:40 -0600

    chore: adding image and link component

--- app-view/components/base/Image.tsx
+++ app-view/components/base/Image.tsx
@@ -1,1 +1,28 @@
-export const Image = () => <div>I'm the image</div>
+import NextImage, { ImageProps } from 'next/image'
+import _ from 'lodash'
+import { buildUrl } from 'cloudinary-build-url';
+
+type CustomImageProps = {
+  src: string,
+  cloudinary?: boolean,
+  props?: any,
+}
+
+export const Image = ({ src, alt, cloudinary, ...props }: CustomImageProps & ImageProps) => {
+  const url = cloudinary ? buildUrl(src, {
+    cloud: {
+      // ToDo: add this from .env
+      cloudName: 'geekli',
+      secure: process.env.NODE_ENV === 'production',
+    },
+  }) : src
+  return (
+    <NextImage
+      src={url}
+      alt={alt}
+      width={1000}
+      height={750}
+      {...props}
+    />
+  )
+}
--- app-view/components/layout/Sidebar.tsx
+++ app-view/components/layout/Sidebar.tsx
@@ -1,1 +1,13 @@
-export const Sidebar = () => <div>I'm on the side</div>
+import { styled } from '../../styles/stitches.config'
+
+export const Sidebar = styled('aside', {
+  minHeight: 'auto',
+  position: 'relative',
+  width: '100%',
+  maxWidth: 480,
+  borderRight: '1px solid $gray-scale-100',
+
+  '@medium': {
+    minHeight: 'calc(100vh - 82px)',
+  }
+})
