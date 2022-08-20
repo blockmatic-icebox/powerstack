b4f0c778b7776189a2bf2b432188666007469b2e
Marco Ledezma <marcoledezmacordero09@gmail.com>
2022-08-20 17:24:48 -0600

    base layout (#15)
    * wip: base layout
    
    * feat: basic base layout

--- app-view/components/layout/Sidebar.tsx
+++ app-view/components/layout/Sidebar.tsx
@@ -1,13 +1,34 @@
 import { styled } from '~/app-view/styles/stitches.config'
+import { BlockmaticIcon } from '~/app-view/components/icons/index'
+import { Link } from '../base/Link'
 
-export const Sidebar = styled('aside', {
+const SidebarContainer = styled('aside', {
+  display: 'none',
   minHeight: 'auto',
   position: 'relative',
   width: '100%',
-  maxWidth: 480,
+  maxWidth: 300,
   borderRight: 'grey 1px solid',
+  '@small': {
+    display: 'block'
+  }
+})
 
-  '@medium': {
-    minHeight: 'calc(100vh - 82px)',
+const Logo = styled(BlockmaticIcon, {
+  height: '21px',
+  width: 'max-content',
+  '@small': {
+    height: '36px',
   },
 })
+
+export const Sidebar = () => {
+  return (
+    <SidebarContainer>
+      <Link href="/">
+        <Logo />
+      </Link>
+      Sidebar Items Here
+    </SidebarContainer>
+  )
+}
