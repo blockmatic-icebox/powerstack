49aac4216b2aa94d48554a1953c20157b94d2c8a
Gabo Esquivel <contact@gaboesquivel.com>
2022-08-19 18:02:35 -0600

    wip: graphql-request client

--- app-engine/graphql/index.ts
+++ app-engine/graphql/index.ts
@@ -1,2 +1,18 @@
 export * from './graphql-client'
 export * as AppGraphQL from './generated-sdk'
+import { app_args } from '~/app-config/app-arguments'
+import { getSdk } from './generated-sdk'
+
+import { GraphQLClient } from 'graphql-request'
+
+// TODO: get jwt with https://www.npmjs.com/package/cookies-next
+const jwt = null
+const headers = jwt
+  ? {
+      // Authorization: `Bearer ${jwt}`,
+      'x-hasura-user-role': 'user',
+    }
+  : {
+      'x-hasura-user-role': 'anon',
+    }
+export const graphql_sdk = getSdk(new GraphQLClient(app_args.services.graphql_api, { headers }))
