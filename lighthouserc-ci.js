1f3d30872903716d06ca9d4598813a624d3da940
djgcr <diego.gamboa.cr@gmail.com>
2022-08-22 17:48:22 -0600

    update lighthouse-ci

--- lighthouserc-ci.js
+++ lighthouserc-ci.js
@@ -4,13 +4,14 @@ module.exports = {
         numberOfRuns: 3,
         startServerCommand: 'yarn run start',
         // staticDistDir "./dist/ng-github-lighthouse-ci",
+        url: ['http://localhost:3000'],
       },
          assert: {
             assertions: {
               'categories:performance': ['error', {minScore: .9}],
               'categories:accessibility': ['error', {minScore: .9}],
               'categories:best-practices': ['error', {minScore: .9}],
-              'categories:seo': ['error', {minScore: .8}],
+              'categories:seo': ['error', {minScore: .9}],
               'categories:pwa': 'off',
             }  
         },
--- lighthouserc.js
+++ lighthouserc.js
@@ -1,16 +1,10 @@
 module.exports = {
     ci: {
       collect: {
-        // staticDistDir 
         startServerCommand: 'yarn run start',
-        url: ['http://localhost:3000']
+        numberOfRuns: 3,
+        url: ['http://localhost:3000'],
       },
-         assert: {
-            assertions: {
-              'categories:performance': ['warn', {minScore: 1}],
-              'categories:accessibility': ['error', {minScore: 1}]
-            }  
-        },
       upload: {
         target: 'temporary-public-storage',
       },
