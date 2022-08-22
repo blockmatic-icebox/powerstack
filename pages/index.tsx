fa2c261145dd5f6f60470db08b234d4f05fd7889
Gabo Esquivel <contact@gaboesquivel.com>
2022-08-22 11:29:39 -0600

    wip: print user data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import { NextPage } from 'next'
+import { useAppEngine } from '~/app-engine'
 
 const HomePage: NextPage = () => {
-  return <p>Home Sweet Home</p>
+  const { user } = useAppEngine()
+  return (
+    <>
+      <h2>Home Sweet Home</h2>
+      <br />
+      <hr />
+      <br />
+      <h3>user data: </h3>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+    </>
+  )
 }
 
 export default HomePage
