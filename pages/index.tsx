b6bcb7a44fc96545ef024a2d528064c7d7742633
Roberto Lucas <andre.rlucas@outlook.com>
2022-08-22 23:06:36 -0600

    impr(wip): state persist

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,14 @@ import { formatAddress } from '~/app-view/library/uiux'
 
 const HomePage: NextPage = () => {
   const { user } = useAppEngine()
+  /** 
+   * NOTE - on refresh after login:
+   * Error: Hydration failed because the initial UI does not match what was rendered on the server.
+   * See more info here: https://nextjs.org/docs/messages/react-hydration-error
+   */
   const user_data = { ...user, jwt: user?.jwt ? formatAddress(user?.jwt) : 'none' }
+  console.log('user_data', user_data)
+
   return (
     <>
       <h2>Home Sweet Home</h2>
