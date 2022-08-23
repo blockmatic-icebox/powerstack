7e61c7b3d35e418fda69efd1db6ab4a61c049f0e
mledezmac <marcoledezmacordero09@gmail.com>
2022-08-23 11:55:46 -0600

    chore: sidebar base markup

--- app-view/components/layout/Sidebar.tsx
+++ app-view/components/layout/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { styled } from '~/app-view/styles/stitches.config'
 import { useAppEngine } from '~/app-engine/index'
+import { Link } from '../base/Link'
 
 const SidebarContainer = styled('aside', {
   display: 'none',
@@ -23,11 +24,36 @@ const SidebarContainer = styled('aside', {
 })
 
 export const Sidebar = () => {
-  const { show_sidebar } = useAppEngine()
+  const { show_sidebar, user } = useAppEngine()
 
   return (
     <SidebarContainer show={show_sidebar}>
-      Sidebar Items Here
+      <div>
+        <header>
+          <h4>Account</h4>
+        </header>
+        <main>
+          <span>image</span>
+          <div>
+            <p><strong>0x4ffc...bBA</strong></p>
+            <p>Metamask Wallet</p>
+          </div>
+          <div>
+            <button type="button">copy</button>
+            <Link href="#">External Address</Link>
+          </div>
+        </main>
+        <footer>
+          <a href="#">+ Connect new wallet</a>
+        </footer>
+      </div>
+      <nav>
+        <ul>
+          <li><Link disabled={user ? true : false} href="/profile">Profile</Link></li>
+          <li><Link disabled={user ? true : false} href="/wallet">Wallet</Link></li>
+          <li><Link disabled={user ? true : false} href="/marketplace">Marketplace</Link></li>
+        </ul>
+      </nav>
     </SidebarContainer>
   )
 }
