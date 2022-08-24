af3cec717009c200359c6708a109f5495296c7f1
mledezmac <marcoledezmacordero09@gmail.com>
2022-08-23 20:20:55 -0600

    feat: adding wallet box

--- app-view/components/layout/Sidebar.tsx
+++ app-view/components/layout/Sidebar.tsx
@@ -5,13 +5,13 @@ import { WalletBox } from '../modules/WalletBox'
 
 const SidebarContainer = styled('aside', {
   display: 'none',
-  minHeight: 'auto',
-  p: '$small $regular',
-  position: 'relative',
-  opacity: 0,
-  width: 0,
+  mb: '$large',
   maxWidth: '100%',
+  minHeight: 'auto',
+  opacity: 0,
+  position: 'relative',
   transition: 'width 400ms, opacity 400ms',
+  width: 0,
   '@small': {
     display: 'block'
   },
@@ -25,19 +25,65 @@ const SidebarContainer = styled('aside', {
   }
 })
 
+const SidebarContent = styled('div', { p: '$small $regular' })
+
+const WalletBoxContainer = styled('div', {
+  background: '#fbfbfb',
+  borderRadius: '$radius-18',
+  p: '$small',
+  minWidth: '100%',
+
+})
+
+const WalletBoxHeader = styled('header', {
+  borderBottom: '1px solid $neutral-100',
+  pb: '$small',
+  mb: '$small',
+})
+
+const WalletBoxTitle = styled('h4', {
+  fontSize: 18,
+  my: 0,
+})
+
+const WalletFooter = styled('footer', {
+  pb: '$x-small',
+  pt: '$xx-small',
+  textAlign: 'center',
+  '& button': {
+    bg: 'transparent',
+    border: 'none',
+    color: '$044-bff',
+    cursor: 'pointer',
+    fontSize: 12,
+    fontWeight: 500,
+    p: 0,
+  }
+})
+
 export const Sidebar = () => {
   const { show_sidebar, user } = useAppEngine()
 
   return (
     <SidebarContainer show={show_sidebar}>
-      {user?.user_addresses.map((user_address, idx) => <WalletBox key={idx} address={user_address.address} />)}
-      <nav>
-        <ul>
-          <li><Link disabled={user ? true : false} href="/profile">Profile</Link></li>
-          <li><Link disabled={user ? true : false} href="/wallet">Wallet</Link></li>
-          <li><Link disabled={user ? true : false} href="/marketplace">Marketplace</Link></li>
-        </ul>
-      </nav>
+      <SidebarContent>
+        <WalletBoxContainer>
+          <WalletBoxHeader>
+            <WalletBoxTitle>Account</WalletBoxTitle>
+          </WalletBoxHeader>
+          {user?.user_addresses.map((user_address, idx) => <WalletBox key={idx} address={user_address.address} />)}
+          <WalletFooter>
+            <button type="button">+ Connect new wallet</button>
+          </WalletFooter>
+        </WalletBoxContainer>
+        <nav>
+          <ul>
+            <li><Link disabled={user ? true : false} href="/profile">Profile</Link></li>
+            <li><Link disabled={user ? true : false} href="/wallet">Wallet</Link></li>
+            <li><Link disabled={user ? true : false} href="/marketplace">Marketplace</Link></li>
+          </ul>
+        </nav>
+      </SidebarContent>
     </SidebarContainer>
   )
 }
--- app-view/components/modules/WalletBox.tsx
+++ app-view/components/modules/WalletBox.tsx
@@ -2,32 +2,18 @@ import Image from 'next/image'
 import { formatAddress } from '~/app-view/library/uiux'
 import { styled } from '~/app-view/styles/stitches.config'
 import { Link } from '../base/Link'
+import { ArrowUpRight, CopyDocumentIcon } from '../icons'
 
 type WalletBoxProps = {
   address: string
 }
 
 const Container = styled('div', {
-  background: '#fbfbfb',
-  borderRadius: '$radius-18',
-  p: '$small',
-})
-
-const Title = styled('h4', {
-  fontSize: 18,
-  my: 0,
-})
-
-const Header = styled('header', {
-  borderBottom: '1px solid $neutral-100',
-  pb: '$small',
-  mb: '$small',
-})
-
-const Main = styled('main', {
   alignItems: 'flex-start',
+  borderBottom: '1px solid $neutral-100',
   display: 'flex',
-
+  mb: '$small',
+  pb: '$small',
 })
 
 const PlaceholderImage = styled('div', {
@@ -35,33 +21,78 @@ const PlaceholderImage = styled('div', {
   mr: '$x-small'
 })
 
+const Address = styled('p', {
+  fontSize: 14,
+  fontWeight: 500,
+  mb: '$xxx-small',
+  mt: 0,
+})
+
+const AddressName = styled('p', {
+  color: '$neutral-300',
+  fontSize: 12,
+  my: 0,
+  '&:before': {
+    display: 'inline-block',
+    bg: '$neutral-300',
+    borderRadius: '50%',
+    content: '""',
+    mr: 2,
+    size: 8,
+  },
+  variants: {
+    isActive: {
+      true: {
+        '&:before': {
+          bg: '$success'
+        }
+      },
+      false: {
+        '&:before': {
+          bg: '$error'
+        }
+      }
+    }
+  }
+})
+
+const AddressActions = styled('div', {
+  alignItems: 'flex-start',
+  display: 'flex',
+  ml: 'auto'
+})
+
+const CopyButton = styled('button', {
+  cursor: 'pointer',
+  bg: 'transparent',
+  border: 'none',
+  mr: '$x-small',
+  p: 0,
+})
+
+const ExternalLink = styled(Link, { mb: 0, mt: 3 })
+
 export const WalletBox = ({ address }: WalletBoxProps) => {
   return (
     <Container>
-      <Header>
-        <Title>Account</Title>
-      </Header>
-      <Main>
-        <PlaceholderImage>
-          <Image
-            src="/wallet-placeholder.jpg"
-            alt="Wallet Placeholder Image"
-            width={21}
-            height={21}
-          />
-        </PlaceholderImage>
-        <div>
-          <p><strong>{formatAddress(address)}</strong></p>
-          <p>Metamask Wallet</p>
-        </div>
-        <div>
-          <button type="button">copy</button>
-          <Link href="#">External Address</Link>
-        </div>
-      </Main>
-      <footer>
-        <a href="#">+ Connect new wallet</a>
-      </footer>
+      <PlaceholderImage>
+        <Image
+          src="/wallet-placeholder.jpg"
+          alt="Wallet Placeholder Image"
+          width={21}
+          height={21}
+        />
+      </PlaceholderImage>
+      <div>
+        <Address>{formatAddress(address)}</Address>
+        {/* ToDo: add the correct name here and state here */}
+        <AddressName isActive={true}>Metamask Wallet</AddressName>
+      </div>
+      <AddressActions>
+        <CopyButton aria-label="Copy Address" type="button"><CopyDocumentIcon /></CopyButton>
+        {/* ToDo: add the correct link here */}
+        <ExternalLink target="_blank" aria-label="Go to address external link" href="#"><ArrowUpRight /></ExternalLink>
+      </AddressActions>
     </Container>
   )
 }
