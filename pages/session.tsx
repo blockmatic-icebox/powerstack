7c6e37465e3a081debadae5cef6b0ac975db0db1
Gabo Esquivel <contact@gaboesquivel.com>
2022-08-24 10:02:01 -0600

    wip: user session

--- pages/session.tsx
+++ pages/session.tsx
@@ -1,17 +1,23 @@
-import { defaultGetServerSideProps, DefaultSsrPage } from '~/app-server/session-ssr'
-import ReactJson from 'react-json-view'
 import { useAppEngine } from '~/app-engine'
+import { defaultGetServerSideProps, DefaultSsrPage } from '~/app-server/session-ssr'
+import { formatAddress } from '~/app-view/library/uiux'
 
 export const getServerSideProps = defaultGetServerSideProps
 
-const Profle: DefaultSsrPage = () => {
+const SessionPage: DefaultSsrPage = () => {
   const { user } = useAppEngine()
+  const user_data = { ...user, jwt: user?.jwt ? formatAddress(user?.jwt) : 'none' }
+
   return (
     <>
-      <h1>My Session Info</h1>
-      {user ? <ReactJson src={user} /> : <p>User not logged in</p>}
+      <h2>My Session</h2>
+      <br />
+      <hr />
+      <br />
+      <h3>user data: </h3>
+      <pre>{JSON.stringify(user_data, null, 2)}</pre>
     </>
   )
 }
 
-export default Profle
+export default SessionPage
