b8f3aaffdba705aad5365ffe2826cb7cc0e66c46
mledezmac <marcoledezmacordero09@gmail.com>
2022-08-25 11:21:04 -0600

    wip: wallet page styles

--- pages/wallet.tsx
+++ pages/wallet.tsx
@@ -1,15 +1,49 @@
 import { NextPage } from 'next'
 import * as Tabs from '@radix-ui/react-tabs';
+import { useAppEngine } from '~/app-engine'
+import { styled } from '~/app-view/styles/stitches.config';
+
+const Title = styled('h2', {
+  fontSize: 24,
+  mb: '$regular',
+  mt: 0,
+})
+
+const TabsTrigger = styled(Tabs.Trigger, {
+  borderBottom: '2px solid transparent',
+  border: 'none',
+  bg: 'transparent',
+  color: '$neutral',
+  cursor: 'pointer',
+  fontSize: 16,
+  fontWeight: 500,
+  mr: '$regular',
+  pb: '$xx-small',
+  transition: 'color 400ms, border-color 400ms',
+  '&[data-state="active"]': {
+    color: '$044-bff',
+    borderBottom: '2px solid $044-bff',
+  }
+})
+
+const TabsList = styled(Tabs.List, {
+  mb: '$regular',
+  pt: '$xx-small',
+})
 
 const Wallet: NextPage = () => {
+  const { user } = useAppEngine()
+
+  console.log(user, 'user')
   return (
     // <h1>You should have your money here 💰</h1>
     <div>
+      <Title>Your Wallet</Title>
       <Tabs.Root defaultValue="tokens" orientation="vertical">
-        <Tabs.List aria-label="Wallet Tabs">
-          <Tabs.Trigger value="tokens">Tokens</Tabs.Trigger>
-          <Tabs.Trigger value="nft-assets">NFTs assets</Tabs.Trigger>
-        </Tabs.List>
+        <TabsList aria-label="Wallet Tabs">
+          <TabsTrigger value="tokens">Tokens</TabsTrigger>
+          <TabsTrigger value="nft-assets">NFTs assets</TabsTrigger>
+        </TabsList>
         <Tabs.Content value="tokens">
           <div>
             <p>Total balance</p>
