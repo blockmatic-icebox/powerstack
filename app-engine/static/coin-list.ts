71e1fcca323c4e3b266d8702fd5492145323837e
Gabo Esquivel <contact@gaboesquivel.com>
2022-08-26 12:27:15 -0600

    wip: zustand ssr and hydration

--- app-engine/static/coin-list.ts
+++ app-engine/static/coin-list.ts
@@ -1,21 +1,19 @@
 import { Coin } from '../types/app-engine'
 
-export const coin_list: Coin[] = [
-  //   {
-  //     name: 'Bitcoin'
-  //   },
-  //   'bitcoin',
-  //   'ethereum',
-  //   'grin',
-  //   'mina-protocol',
-  //   'solana',
-  //   'polkadot',
-  //   'binancecoin',
-  //   'avalanche-2',
-  //   'basic-attention-token',
-  //   'immutable-x',
-  //   'flow',
-  //   'eos',
-  //   'aave',
-  //   'snx',
+export const coingecko_key_list = [
+  'bitcoin',
+  'ethereum',
+  'grin',
+  'mina-protocol',
+  'solana',
+  'polkadot',
+  'binancecoin',
+  'avalanche-2',
+  'basic-attention-token',
+  'immutable-x',
+  'flow',
+  'eos',
+  'aave',
+  'snx',
 ]
+export const coin_list: Coin[] = []
