c4fb911c666abba86d38a9080633af7e20b40a96
Gabo Esquivel <contact@gaboesquivel.com>
2022-08-26 00:28:36 -0600

    wip: coingecko service

---
+++ app-engine/services/coingecko.ts
@@ -0,0 +1,31 @@
+import { app_logger } from '../library/logger'
+import { CoinGeckoClient } from 'coingecko-api-v3'
+
+const coingeck_client = new CoinGeckoClient({
+  timeout: 10000,
+  autoRetry: true,
+})
+
+const coins = [
+  'bitcoin',
+  'ethereum',
+  'grin',
+  'mina-protocol',
+  'solana',
+  'polkadot',
+  'binancecoin',
+  'avalanche-2',
+  'binance-usd',
+  'basic-attention-token',
+  'immutable-x',
+  'flow',
+  'eos',
+  'aave',
+  'snx',
+]
+
+const getCoinGeckoPrices = async () => {
+  const response = await coingeck_client.simplePrice({ ids: String(coins), vs_currencies: 'usd' })
+  app_logger.log('got coingecko prices', response)
+  return response
+}
