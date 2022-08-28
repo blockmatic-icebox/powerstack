6fade8029bb99973ad81babc0382e174505ea809
Ronny Delgado <delgado.ronny@gmail.com>
2022-08-28 16:04:25 -0600

    refactor: remove lighthouse assertions to to leave a basic configuration

--- lighthouserc-ci.js
+++ lighthouserc-ci.js
@@ -5,18 +5,8 @@ module.exports = {
         startServerCommand: 'yarn start',
         url: ['http://localhost:3000/'],
       },
-         assert: {
-          preset:  "lighthouse:no-pwa",
-            assertions: {
-              'categories:performance': ['error', {minScore: .9}],
-              'categories:accessibility': ['error', {minScore: .9}],
-              'categories:best-practices': ['error', {minScore: .9}],
-              'categories:seo': ['error', {minScore: .9}],
-              'categories:pwa': 'off',
-            },  
-        },
-        upload: {
-          target: 'temporary-public-storage',
+      upload: {
+        target: 'temporary-public-storage',
       },
     },
   };
