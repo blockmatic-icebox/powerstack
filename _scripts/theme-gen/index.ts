56c4054e15bed3079aaead81a43c1cde177033b4
Roberto Lucas <andre.rlucas@outlook.com>
2022-08-30 00:01:44 -0600

    feat(wip): toolabs ds theme gen

--- _scripts/theme-gen/index.ts
+++ _scripts/theme-gen/index.ts
@@ -5,85 +5,96 @@ import fetch from 'cross-fetch'
 import fs from 'fs'
 import path from 'path'
 
-const Spinner = require('cli-spinner').Spinner
+import { Spinner } from 'cli-spinner'
+
+type TypeStyles = Toolabs.Maybe<{
+  fontFamily: string
+  fontSize: string
+  lineHeight: string
+  letterSpacing: string
+}>
+type Theme = Toolabs.Maybe<Toolabs.GetMyThemesQuery> & {
+  typeStyles: TypeStyles
+}
+type ThemeKey = keyof Theme
 
 interface ToolabsTheme {
   name: string
-  theme: {
-    color: Toolabs.Color[]
-    gradients: Toolabs.Gradient[]
-  }
+  theme: Theme
 }
 
 const spinner_string = '⠋⠙⠹⠸⠼⠴⠦⠧⠇⠏'
-const pre_selected_themes = process.env.THEME_GEN_THEMES
+const selected_themes = process.env.THEME_GEN_THEMES ?? 'Default'
 
-// TODO: Interface for themes
-const themes: ToolabsTheme = {}
+const themes: ToolabsTheme[] = []
 
 // NOTE: Reading how many themes do we have
 const themes_spinner = setSpinner(` %s 👀 for themes...
  .
  `)
-themes_spinner.setSpinnerString(spinner_string).setSpinnerDelay(80)
+themes_spinner.setSpinnerString(spinner_string)
 themes_spinner.start()
 
+// Looking for theme Color Modes if any
+let fetched_themes: Toolabs.Maybe<Toolabs.Maybe<Toolabs.ThemeState>[]> = [];
+  
 (async () => {
   const { data, error } = await getThemes()
 
   if (error) throw new Error(error)
 
-  const theme_ids = data.filter(theme => theme.name === 'Color Mode').map(theme => theme.variants)[0]
-
-  themes[]
+  // Any possible match for the Light/Dark Themes names...
+  fetched_themes = data.filter(theme => theme.name.toLowerCase().match(/^(color mode|theme color mode|colors|modes)$/)).map(theme => theme.variants)[0] || []
+})();
+(() => {
+  selected_themes.split(',').forEach(async theme => {
+    const { data, error } = await getThemeById(theme)
+    const theme_name = fetched_themes?.find(t => t?.id === theme)?.name ?? 'Default'
+    
+    if (error) throw new Error(error)
+    
+    themes.push({
+      name: theme_name,
+      theme: data as Theme,
+    })
+  })
 })()
 
-themes_spinner.stop()
-
-// TODO: From here it breaks
-fs.readdirSync(dir).forEach((json) => {
-  const theme_key = json.replace(/(-theme\.json|\.json)/, '').replace(/theme/, 'default')
-  file_names.push(theme_key)
-  console.log(`🖌️  ${theme_key} theme found!`)
-
-  themes[theme_key] = require(path.join(dir, json))
-})
-
-themes_spinner.stop()
-const toolabs_theme_spinner = setSpinner(`
- %s 〰️ Processing Toolabs JSON Themes...`)
-toolabs_theme_spinner.setSpinnerString(spinner_string).setSpinnerDelay(80)
+themes_spinner.stop();
+console.log('\n\n')
+const toolabs_theme_spinner = setSpinner(`%s 〰️ Processing Toolabs JSON Themes...`)
+toolabs_theme_spinner.setSpinnerString(spinner_string)
 toolabs_theme_spinner.start()
 
-const new_themes = []
+const new_themes: ToolabsTheme[] = []
+const icons: Toolabs.Icon[] = []
 
-Object.keys(themes).forEach((theme) => {
-  const new_theme = {
-    name: themes[theme].name,
+themes.forEach((theme) => {
+  const new_theme: ToolabsTheme = {
+    name: theme.name,
     theme: {
-      typeStyles: {},
-      radii: {},
-      shadows: {},
+      typeStyles: {} as TypeStyles,
     },
   }
-  const theme_object = themes[theme]
+  const theme_object: Theme = theme.theme
 
   Object.keys(theme_object).forEach((t_key) => {
-    new_theme.theme[t_key] = {}
+    // @ts-ignore
+    new_theme.theme[t_key as keyof ThemeKey] = {} as any
 
     switch (t_key) {
-      case 'name':
-        new_theme[t_key] = theme_object[t_key]
-        break
       case 'typeStyles':
         let typeStyleLabel = ''
 
-        theme_object[t_key].forEach((typeStyle) => {
+        // @ts-ignore
+        theme_object[t_key as ThemeKey]!.forEach((typeStyle: any) => {
           Object.keys(typeStyle).forEach((style) => {
             if (style === 'name') {
               typeStyleLabel = typeStyle[style]
             } else {
+              // @ts-ignore
               new_theme.theme.typeStyles[typeStyleLabel] = {
+                // @ts-ignore
                 ...new_theme.theme.typeStyles[typeStyleLabel],
                 [style]: typeStyle[style],
               }
@@ -91,39 +102,43 @@ Object.keys(themes).forEach((theme) => {
           })
         })
         break
-      case 'BorderRadiuses': {
-        theme_object[t_key].forEach((prop) => {
-          new_theme.theme.radii[prop.name] = prop.value
-        })
-        break
-      }
       case 'easeCurves': {
-        theme_object[t_key].forEach((prop) => {
-          new_theme.theme[t_key][prop.name] = prop.curve
+        theme_object[t_key]!.forEach((prop) => {
+          // @ts-ignore
+          new_theme.theme[t_key as ThemeKey][prop.name] = prop.curve
         })
         break
       }
       case 'durations':
-        theme_object[t_key].forEach((prop) => {
-          new_theme.theme[t_key][prop.name] = prop.duration
+        theme_object[t_key]!.forEach((prop) => {
+          // @ts-ignore
+          new_theme.theme[t_key as ThemeKey][prop.name] = prop.duration
         })
         break
       case 'fonts':
-        theme_object[t_key].forEach((prop) => {
-          new_theme.theme[t_key][prop.name] = prop.fontFamily
+        // @ts-ignore
+        theme_object[t_key]!.forEach((prop) => {
+          // @ts-ignore
+          new_theme.theme[t_key as ThemeKey][prop.name] = prop.fontFamily
         })
         break
-      case 'Shadows':
-        theme_object[t_key].forEach((prop) => {
+      case 'shadows':
+        theme_object[t_key]!.forEach((prop) => {
+          // @ts-ignore
           new_theme.theme.shadows[prop.name] = prop.value
         })
         break
+      case 'radii':
       case 'colors':
-      case 'fonts':
       case 'space':
-        theme_object[t_key].forEach((prop) => {
-          new_theme.theme[t_key][prop.name] = prop.value
+        // @ts-ignore
+        theme_object[t_key]!.forEach((prop) => {
+          // @ts-ignore
+          new_theme.theme[t_key as ThemeKey][prop.name] = prop.value
         })
+        break
+      case 'icons':
+        icons = theme_object[t_key] as Toolabs.Icon[]
         break
       default:
         break
@@ -131,6 +146,7 @@ Object.keys(themes).forEach((theme) => {
   })
 
   Object.keys(new_theme.theme).forEach((key) => {
+    // @ts-ignore
     if (Object.keys(new_theme.theme[key]).length === 0) delete new_theme.theme[key]
   })
 
@@ -140,17 +156,15 @@ Object.keys(themes).forEach((theme) => {
 toolabs_theme_spinner.stop()
 console.log(`
 ✔️  Toolabs JSON Themes Digested successfully 🪄🎉`)
-const writing_theme_spinner = setSpinner(` %s 〰️ Writting Toolabs JSON Theme for stitches...
-
-`)
-writing_theme_spinner.setSpinnerString(spinner_string).setSpinnerDelay(80)
+const writing_theme_spinner = setSpinner(` %s 〰️ Writting Toolabs JSON Theme for stitches...`)
+writing_theme_spinner.setSpinnerString(spinner_string)
 writing_theme_spinner.start()
 
-file_names.forEach((file) => {
+new_themes.forEach(({ name, theme }) => {
   fs.writeFile(
-    `./app-view/styles/themes/${file !== 'default' ? `${file}-` : ''}theme.ts`,
-    `export const ${file !== 'default' ? `${file}_` : ''}theme = ${JSON.stringify(
-      new_themes.find((t) => t.name === file),
+    `./app-view/styles/themes/${name !== 'default' ? `${name}-` : ''}theme.ts`,
+    `export const ${name !== 'default' ? `${name}_` : ''}theme = ${JSON.stringify(
+      { name, theme },
       null,
       2,
     )}`,
@@ -160,15 +174,17 @@ file_names.forEach((file) => {
         writing_theme_spinner.stop()
         throw new Error(
           `
-          ❌ There was problem trying to creting the file 💔. Check if values are valid.`,
-        )
-      }
+          ❌ There was problem trying to creating the file 💔. Check if values are valid.`,
+          )
+        }
 
-      writing_theme_spinner.stop()
-      console.log(`✔️  Stitches file for ${file} theme created successfully 🪄🎉`)
+        writing_theme_spinner.stop()
+      console.log(`✔️  Stitches file for ${name} theme created successfully 🪄🎉`)
     },
-  )
+    )
 })
+
+writing_theme_spinner.stop()
 
 // TODO: From here it is not crashing...
 
@@ -193,7 +209,7 @@ function toolabsClient() {
 
 async function getThemes(): Promise<{ data: Toolabs.Theme[]; error: string;  }> {
   try {
-    const { data, errors } = await toolabsClient().query<
+    const { data, error } = await toolabsClient().query<
       Toolabs.ThemeGenThemesQuery,
       Toolabs.ThemeGenThemesQueryVariables
     >({
@@ -202,12 +218,35 @@ async function getThemes(): Promise<{ data: Toolabs.Theme[]; error: string;  }> 
 
     console.log('results toolabs by getting themes =>>>', JSON.stringify(data, null, 2))
 
-    if (errors) throw new Error(JSON.stringify(errors, null, 2))
+    if (error) throw new Error(JSON.stringify(error, null, 2))
 
     return { data: data.themes as Toolabs.Theme[], error: '' }
   } catch (error) {
     console.log('(error as Error).message', (error as Error).message)
     return { data: [] as Toolabs.Theme[], error: (error as Error).message }
+  }
+}
+
+async function getThemeById(theme_id: string): Promise<{ data: Toolabs.GetMyThemesQuery; error: string; }> {
+  try {
+    const { data, error } = await toolabsClient().query<
+      Toolabs.GetMyThemesQuery,
+      Toolabs.GetMyThemesQueryVariables
+    >({
+      query: Toolabs.GetMyThemesDocument,
+      variables: {
+        themes: theme_id
+      }
+    })
+    
+    console.log(`results toolabs by getting ${theme_id} theme =>>>`, JSON.stringify(data, null, 2))
+
+    if (error) throw new Error(JSON.stringify(error, null, 2))
+
+    return { data: data as Toolabs.GetMyThemesQuery, error: '' }
+  } catch (error) {
+    console.log('(error as Error).message', (error as Error).message)
+    return { data: {} as Toolabs.GetMyThemesQuery, error: (error as Error).message }
   }
 }
 
