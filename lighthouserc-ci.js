0aa0b73e67198a3d6c6bc2aa7f66a6f0af29eedc
djgcr <diego.gamboa.cr@gmail.com>
2022-08-31 17:04:25 -0600

    Lighthouse asserts

--- lighthouserc-ci.js
+++ lighthouserc-ci.js
@@ -2,11 +2,27 @@ module.exports = {
     ci: {
       collect: {
         numberOfRuns: 3,
-        startServerCommand: 'yarn build && start',
+        startServerCommand: 'yarn start',
         url: ['http://localhost:3000/'],
       },
+      assert: {
+        preset: 'lighthouse:recommended',
+        assertions: {
+            "categories:performace": ["error", {"minScore": 0.8}],
+            "categories:accessibility": ["error", {"minScore": 0.8}],
+            "categories:best-practices": ["error", {"minScore": 0.8}],
+            "categories:seo": ["error", {"minScore": 0.8}],
+            "apple-touch-icon": "off",
+            "maskable-icon": "off",
+            "installable-manifest": "off",
+            "service-worker": "off",
+            "splash-screen": "off",
+            "themed-omnibox": "off",
+        }
+      },  
       upload: {
         target: 'temporary-public-storage',
       },
     },
-  };
+}
+
