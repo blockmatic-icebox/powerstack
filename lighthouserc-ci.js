25913ba3e0e1617612fcb81784b6f6cf514b1114
Ronny Delgado <delgado.ronny@gmail.com>
2022-08-31 17:23:01 -0600

    wip: add more settings

--- lighthouserc-ci.js
+++ lighthouserc-ci.js
@@ -6,8 +6,18 @@ module.exports = {
       numberOfRuns: 3,
       startServerCommand: 'sudo yarn run start -p 3000',
       url: ['http://localhost:3000'],
-      disableDevShmUsage: true,
-      maxWaitForFcp: 45000,
+      settings: {
+        // Don't run certain audits
+        skipAudits: ['redirects-http'],
+        // Don't clear localStorage/IndexedDB/etc before loading the page.
+        disableStorageReset: true,
+        // Wait up to 90s for the page to load
+        maxWaitForLoad: 90000,
+        // Use applied throttling instead of simulated throttling
+        throttlingMethod: 'devtools',
+        disableDevShmUsage: true,
+        maxWaitForFcp: 45000,
+      },
     },
     upload: {
       //target: 'temporary-public-storage',
