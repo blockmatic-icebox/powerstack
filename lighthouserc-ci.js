022f493b5b4daa7ab9eb7b1fd32f2e6a0a421103
Ronny Delgado <delgado.ronny@gmail.com>
2022-08-31 17:18:29 -0600

    wip: test with disableDevShmUsage property

--- lighthouserc-ci.js
+++ lighthouserc-ci.js
@@ -4,9 +4,9 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 3,
-      startServerCommand: 'sudo yarn run start -p 8080',
-      url: ['http://localhost:8080'],
-      //disableDevShmUsage: true,
+      startServerCommand: 'sudo yarn run start -p 3000',
+      url: ['http://localhost:3000'],
+      disableDevShmUsage: true,
       maxWaitForFcp: 45000,
     },
     upload: {
@@ -14,6 +14,5 @@ module.exports = {
       target: 'filesystem',
       output: '/tmp/artifacts',
     },
-    maxWaitForFcp: 45000,
   },
 }
