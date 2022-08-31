c83771fe3038c82ede551528952deec2b6ea1f2e
Ronny Delgado <delgado.ronny@gmail.com>
2022-08-31 17:14:03 -0600

    wip: add maxWaitForFcp setting

--- lighthouserc-ci.js
+++ lighthouserc-ci.js
@@ -1,14 +1,19 @@
+const { truncate } = require('fs')
+
 module.exports = {
   ci: {
     collect: {
       numberOfRuns: 3,
       startServerCommand: 'sudo yarn run start -p 8080',
       url: ['http://localhost:8080'],
+      //disableDevShmUsage: true,
+      maxWaitForFcp: 45000,
     },
     upload: {
       //target: 'temporary-public-storage',
       target: 'filesystem',
       output: '/tmp/artifacts',
     },
+    maxWaitForFcp: 45000,
   },
 }
