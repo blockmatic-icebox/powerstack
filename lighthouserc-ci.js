2d6ff178639d9dbc389c880199dea3b7cd7b0974
Ronny Delgado <delgado.ronny@gmail.com>
2022-08-31 17:36:07 -0600

    wip: add scripts in yml file

--- lighthouserc-ci.js
+++ lighthouserc-ci.js
@@ -4,20 +4,20 @@ module.exports = {
   ci: {
     collect: {
       numberOfRuns: 3,
-      startServerCommand: 'sudo yarn run start -p 3000',
-      url: ['http://localhost:3000'],
-      settings: {
-        // Don't run certain audits
-        skipAudits: ['redirects-http'],
-        // Don't clear localStorage/IndexedDB/etc before loading the page.
-        disableStorageReset: true,
-        // Wait up to 90s for the page to load
-        maxWaitForLoad: 90000,
-        // Use applied throttling instead of simulated throttling
-        throttlingMethod: 'devtools',
-        disableDevShmUsage: true,
-        maxWaitForFcp: 45000,
-      },
+      startServerCommand: 'yarn start -p 3000',
+      url: ['http://localhost:3000/'],
+      //   settings: {
+      //     // Don't run certain audits
+      //     skipAudits: ['redirects-http'],
+      //     // Don't clear localStorage/IndexedDB/etc before loading the page.
+      //     disableStorageReset: true,
+      //     // Wait up to 90s for the page to load
+      //     maxWaitForLoad: 90000,
+      //     // Use applied throttling instead of simulated throttling
+      //     throttlingMethod: 'devtools',
+      //     disableDevShmUsage: true,
+      //     maxWaitForFcp: 45000,
+      //   },
     },
     upload: {
       //target: 'temporary-public-storage',
