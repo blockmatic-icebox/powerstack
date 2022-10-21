4963f1491bc05f0db1d88377a19881452e7d00ca
djgcr <diego.gamboa.cr@gmail.com>
2022-10-21 17:11:02 -0600

    update engine/auth.service

--- engine/_legacy/store/session-slice.ts
+++ engine/_legacy/store/session-slice.ts
@@ -1,7 +1,7 @@
 import type { StoreSlice } from './index'
 import { AppLoginMethod } from '../types/engine.type'
 import { app_logger } from '../library/logger.lib'
-import { auth_server } from '../services/auth-server'
+import { auth_server } from '../services/auth.service'
 import { AuthErrorResponse } from '~/server/session-auth'
 
 export interface SessionState {
