030ef5175b943ce22bf81b2d8ab64bc316af14cf
djgcr <diego.gamboa.cr@gmail.com>
2022-10-28 12:49:29 -0600

    create SidebarLink

---
+++ ui/components/SidebarLink.ts
@@ -0,0 +1,18 @@
+const menuItems = [
+    {
+        href: '/',
+        title: 'Homepage',
+    },
+    {
+        href: '/profile',
+        title: 'Profile',
+    },
+    {
+        href: '/wallet',
+        title: 'Wallet',
+    },
+    {
+        href: '/marketplace',
+        title: 'Marketplace',
+    }, 
+  ];
