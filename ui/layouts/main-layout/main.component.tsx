06483a06a7eb98a5ac27c42764ae59ee1ec0f1a6
djgcr <diego.gamboa.cr@gmail.com>
2022-10-31 08:53:08 -0600

    fix: main component

--- ui/layouts/main-layout/main.component.tsx
+++ ui/layouts/main-layout/main.component.tsx
@@ -4,7 +4,8 @@ export function Main({ children }: { children: React.ReactNode }) {
         <div className="py-6">
           <div className="px-4 max-w-7xl sm:px-6 md:px-8">
             <div className="py-4">
-              <div className="border-4 border-gray-200 border-dashed rounded-lg h-96">{children}</div>
+              <div>{children}</div>
+              <div className="border-4 border-gray-200 border-dashed rounded-lg h-96"></div>
             </div>   
           </div>
         </div>
