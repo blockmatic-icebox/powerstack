a1cfadccd2f3927ce9af66ab79f1bda71e0c2514
djgcr <diego.gamboa.cr@gmail.com>
2022-11-01 18:46:05 -0600

    feat: search-bar header

--- ui/icons/index.ts
+++ ui/icons/index.ts
@@ -1,2 +1,3 @@
 export { default as BlockmaticIcon } from './BlockmaticIcon'
 export { default as Blockmatic } from './Blockmatic'
+export { default as SparkleLight } from './SparkleLight'
--- ui/layouts/header/header.component.tsx
+++ ui/layouts/header/header.component.tsx
@@ -4,6 +4,7 @@ import { CheckIcon, ChevronUpDownIcon, MagnifyingGlassIcon } from '@heroicons/re
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import BlockmaticIcon from '~/ui/icons/BlockmaticIcon'
 import { Blockmatic } from '~/ui/icons'
+import { SearchBar } from '~/ui/components/search-bar/search-bar.component'
 // import { SidebarLink } from "./sidebar-link.component"
 
 const user = {
@@ -29,21 +30,14 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
 
-const coins = [
-  { id: 1, name: 'Ethereum' },
-  { id: 2, name: 'Solana' },
-  { id: 3, name: 'Polygon' },
-  { id: 4, name: 'EOS' },
-]
 
 
 export function Header() {
-  const [selected, setSelected] = useState(coins[3])
-
+ 
   return (
     <div className="min-h-full">
-      <div className="bg-white lg:border-b lg:border-indigo-300 lg:border-opacity-25">
-        <Disclosure as="nav" className="border-b border-indigo-300 border-opacity-25 lg:border-none ">
+      <div className="bg-white sm:border-b sm:border-indigo-300 sm:border-opacity-25">
+        <Disclosure as="nav" className="">
           {({ open }) => (
             <>
               <div className="px-2 mx-auto max-w-7xl sm:px-4 lg:px-8">
@@ -56,86 +50,9 @@ export function Header() {
                       <Blockmatic />
                     </div>
 
-                    <div className="hidden bg-gray-200 lg:ml-10 md:block ">
-{/* search */}
-                      <div className="flex items-stretch w-full max-w-3xl md:max-w-xs">
-                        <label
-                          htmlFor="search"
-                          className="sr-only">
-                          Search
-                        </label>
-                        <div className="relative flex text-gray-400 focus-within:text-gray-600">
-                          <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <MagnifyingGlassIcon className="w-5 h-5" aria-hidden="true" />
-                          </div>
-                          <input
-                            id="search"
-                            className="block w-full py-2 pl-10 pr-3 leading-5 text-gray-900 placeholder-gray-500 border border-transparent rounded-md focus:border-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:text-sm"
-                            placeholder="Search by Address / Txn Hash"
-                            type="search"
-                            name="search"
-                          />
-{/* coins */}
-                          <Listbox value={selected} onChange={setSelected}>
-                            {({ open }) => (
-                              <>
-                                <div className="relative mt-1">
-                                  <Listbox.Button className="relative w-full py-2 pl-3 pr-10 text-left border border-gray-300 rounded-md shadow-sm cursor-default focus:border-white focus:outline-none focus:ring-1 sm:text-sm">
-                                    <span className="block truncate">{selected.name}</span>
-                                    <span className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                                      <ChevronUpDownIcon className="w-5 h-5 text-gray-400" aria-hidden="true" />
-                                    </span>
-                                  </Listbox.Button>
-
-                                  <Transition
-                                    show={open}
-                                    as={Fragment}
-                                    leave="transition ease-in duration-100"
-                                    leaveFrom="opacity-100"
-                                    leaveTo="opacity-0"
-                                  >
-                                    <Listbox.Options className="absolute z-10 w-auto py-1 mt-1 overflow-auto text-base bg-white shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm ">
-                                      {coins.map((person) => (
-                                        <Listbox.Option
-                                          key={person.id}
-                                          className={({ active }) =>
-                                            classNames(
-                                              active ? 'text-black bg-gray-100' : 'text-gray-900',
-                                              'relative cursor-default select-none py-2 pl-3 pr-9'
-                                            )
-                                          }
-                                          value={person}
-                                        >
-                                          {({ selected, active }) => (
-                                            <>
-                                              <span className={classNames(selected ? 'font-semibold' : 'font-normal', 'block truncate')}>
-                                                {person.name}
-                                              </span>
-
-                                              {selected ? (
-                                                <span
-                                                  className={classNames(
-                                                    active ? 'text-white' : 'text-black',
-                                                    'absolute inset-y-0 right-0 flex items-center pr-4'
-                                                  )}
-                                                >
-                                                  <CheckIcon className="w-5 h-5" aria-hidden="true" />
-                                                </span>
-                                              ) : null}
-                                            </>
-                                          )}
-                                        </Listbox.Option>
-                                      ))}
-                                    </Listbox.Options>
-                                  </Transition>
-                                </div>
-                              </>
-                            )}
-                          </Listbox>
-                        </div>
-                      </div>
-                      
-                    </div>
+                  <SearchBar />
+                    
+    
                   </div>
                   <div className="flex justify-center flex-1 px-2 lg:ml-6 lg:justify-end">
 
