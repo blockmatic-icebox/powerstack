f45b676f65d557639c3a51cd4d2927673add23b7
diegogqcr <diego.gamboa.cr@gmail.com>
2022-11-10 13:30:28 -0600

    feat: create profile route

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,7 @@
-export default function Page({ children }: { children: React.ReactNode }) {
-    return (
-      <html lang="en">
-        <head>
-          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-          <meta name="description" content="Blockmatic"></meta>
-          <title>Blockmatic</title>
-        </head>
-        <body className="w-full h-screen">
-              <h1>Home Page</h1>
-              {children}
-        </body>
-      </html>
-    )
-  }
+export default function Home() {
+  return (
+    <div>
+     <p className="bg-red-600 ">Home Page</p>
+    </div>
+  )
+}
---
+++ app/profile/layout.tsx
@@ -0,0 +1,12 @@
+export default function ProfileLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <div>     
+          {children}
+      </div>
+    )
+  }
+  
