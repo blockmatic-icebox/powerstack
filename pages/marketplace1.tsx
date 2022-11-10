7e79c29ed2f7e7f10412720b86b095090f2adc89
diegogqcr <diego.gamboa.cr@gmail.com>
2022-11-10 16:00:08 -0600

    feat: remove page from old route

--- pages/marketplace1.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Marketplace() {
-  return (
-    <div>
-      <h2>Marketplace</h2>
-    </div>
-  )
-}
--- pages/profile1.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Profile() {
-  return (
-   <div>
-      <h2>Profile</h2>
-   </div>
-  )
-}
