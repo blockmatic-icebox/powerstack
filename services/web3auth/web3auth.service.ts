feae772f885ec62e6f4de0de02315fcd9becf45f
Gabo Esquivel <contact@gaboesquivel.com>
2022-12-19 12:04:56 -0600

    wip: add web3auth service

---
+++ services/web3auth/web3auth.service.ts
@@ -0,0 +1,157 @@
+import { ADAPTER_EVENTS, WALLET_ADAPTERS } from '@web3auth/base'
+import { logger } from '~/lib/logger/logger.lib'
+import { clientEnv } from '~/config/client/client-env.config'
+import { chainConfig } from '~/config/chain/chain.config'
+import { OpenloginAdapter } from '@web3auth/openlogin-adapter'
+import { Web3AuthCore } from '@web3auth/core'
+import { SolanaWallet } from '@web3auth/solana-provider'
+import { PhantomAdapter } from '@web3auth/phantom-adapter'
+
+function subscribeToWeb3AuthEvents() {
+  const web3auth = window.web3auth!
+  if (!web3auth) throw new Error('web3auth not initialized')
+
+  web3auth.on(ADAPTER_EVENTS.CONNECTED, async (data: unknown) => {
+    const web3AuthUser = await web3auth.getUserInfo()
+    logger.log('🔑 you are successfully logged in', web3AuthUser, data)
+  })
+
+  web3auth.on(ADAPTER_EVENTS.CONNECTING, () => logger.log('connecting ...'))
+
+  web3auth.on(ADAPTER_EVENTS.DISCONNECTED, () => logger.log('🔑 web3auth disconnected'))
+
+  web3auth.on(ADAPTER_EVENTS.ERRORED, (error: unknown) =>
+    logger.error('🔑 web3auth some error or user has cancelled login request', error),
+  )
+}
+
+export async function initializeWeb3Auth() {
+  logger.log('🔑 initializing web3auth ...')
+
+  // configure the client
+  const web3auth = new Web3AuthCore({
+    clientId: clientEnv.web3auth.clientId,
+    chainConfig: chainConfig.solanaTestnet,
+  })
+
+  // make it available globally
+  window.web3auth = web3auth
+
+  // subscribe to events
+  subscribeToWeb3AuthEvents()
+
+  // open login adapter
+  const adapter = new OpenloginAdapter({
+    adapterSettings: {
+      network: 'testnet',
+      clientId: clientEnv.web3auth.clientId,
+      uxMode: 'redirect',
+      loginConfig: {
+        jwt: {
+          name: 'Login',
+          verifier: clientEnv.web3auth.verifier,
+          typeOfLogin: 'jwt',
+          clientId: '',
+        },
+      },
+    },
+  })
+
+  web3auth.configureAdapter(adapter)
+
+  // configure phantom wallet adapter
+  const phantomAdapter = new PhantomAdapter({
+    clientId: clientEnv.web3auth.clientId,
+  })
+
+  web3auth.configureAdapter(phantomAdapter)
+
+  // initialize web3auth instance
+  await web3auth.init()
+
+  logger.log(`🔑 web3auth initialized`)
+}
+
+export async function getWeb3AuthUser() {
+  let web3AuthUser
+  try {
+    web3AuthUser = await window.web3auth?.getUserInfo()
+  } catch (error) {
+    logger.error(error)
+    throw new Error('User not logged in')
+  } finally {
+    if (!web3AuthUser) throw new Error('User not logged in')
+    return web3AuthUser
+  }
+}
+
+async function getWeb3AuthBeforeLogin() {
+  const web3auth = getWeb3Auth()
+  // check if user already logged in.
+  let web3AuthUser
+  try {
+    web3AuthUser = await web3auth.getUserInfo()
+    if (web3AuthUser) logger.log('user already logged in', web3AuthUser)
+  } catch (error) {
+    // this is fine.
+  }
+  return { web3auth, web3AuthUser }
+}
+
+export async function loginWithMetamask() {
+  logger.log('🔑 login with phantom')
+  const { web3auth, web3AuthUser } = await getWeb3AuthBeforeLogin()
+  await web3auth.connectTo(WALLET_ADAPTERS.METAMASK)
+  if (web3AuthUser) return
+}
+
+export async function loginWithPhantom() {
+  logger.log('🔑 login with phantom')
+  const { web3auth, web3AuthUser } = await getWeb3AuthBeforeLogin()
+  await web3auth.connectTo(WALLET_ADAPTERS.PHANTOM)
+  if (web3AuthUser) return
+}
+
+export async function loginWithGoogle() {
+  const { web3auth, web3AuthUser } = await getWeb3AuthBeforeLogin()
+  if (web3AuthUser) return
+  // login with google. the auth server redirects back to the current page.
+  try {
+    await web3auth.connectTo(WALLET_ADAPTERS.OPENLOGIN, {
+      relogin: true,
+      loginProvider: 'jwt',
+      extraLoginOptions: {
+        domain: `https://localhost/oauth2`,
+        verifierIdField: 'email',
+        response_type: 'token',
+        scope: 'email openid profile',
+      },
+    })
+  } catch (error) {
+    logger.error(error)
+    alert('an error has occurred')
+  }
+}
+
+export async function logout() {
+  const web3auth = getWeb3Auth()
+  return web3auth?.logout()
+}
+
+export function getWeb3Auth() {
+  const web3auth = window.web3auth
+  if (!web3auth) throw new Error('web3auth not initialized')
+  return web3auth
+}
+
+export function getWeb3AuthProvider() {
+  const web3auth = getWeb3Auth()
+  if (!web3auth.provider) throw new Error('web3auth provider not found')
+  return web3auth.provider
+}
+
+// utility fn, wont broadcast
+export async function getWalletAddresses() {
+  const solanaWallet = new SolanaWallet(getWeb3AuthProvider())
+  return solanaWallet.requestAccounts()
+}
--- types/global/global.type.ts
+++ types/global/global.type.ts
@@ -1,5 +1,8 @@
+import { Web3AuthCore } from '@web3auth/core'
+
 declare global {
   interface Window {
+    web3auth: Web3AuthCore
     ethereum: any
     VConsole?: any
     prisma: any
