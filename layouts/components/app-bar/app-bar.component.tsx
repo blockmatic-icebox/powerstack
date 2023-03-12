025ba5ee9db489a693a3d0eb49422b48a76ca7d7
Gabo Esquivel <contact@gaboesquivel.com>
2023-03-12 09:05:01 -0600

    wip: hamburger :P

--- layouts/components/app-bar/app-bar.component.tsx
+++ layouts/components/app-bar/app-bar.component.tsx
@@ -1,5 +1,10 @@
+import { Hamburger } from '../hamburger'
+
 export function AppBar() {
   return (
-    <div className="sticky top-0 z-10 flex flex-col w-full bg-white md:items-center">app bar</div>
+    <div className="sticky top-0 z-10 flex flex-col w-full bg-white md:items-center">
+      app bar
+      <Hamburger />
+    </div>
   )
 }
